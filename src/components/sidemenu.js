73d5e2fcdb558be6f589afa9b44c7ec5e15d7d46
nadeesha <nadeeshamadhushan9@gmail.com>
2021-09-10 07:48:42 +0530

    temp

---
+++ src/components/sidemenu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const styles={
+    sideMenu:{
+        display:'flex',
+        flexDirection:'column',
+        position
+    }
+}
+
+export default function Sidemenu() {
+    return (
+        <div className="side-menu">
+            
+        </div>
+    )
+}
